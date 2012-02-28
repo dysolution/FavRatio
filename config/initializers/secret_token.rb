fd604ba2400746d41630e4792d45f35fac5822fe
Jordan Peterson <dysolution@gmail.com>
2012-02-28 12:39:10 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favratio::Application.config.secret_token = '3384d595d9436fe3880464e8a75a807a609b5ae3c96e3fb750fe40182907d5abd006303535c5b759afc1440d720c16bfe2cfbd01961db755658fbcf0b1225c12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Favratio::Application.config.session_store :cookie_store, key: '_favratio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Favratio::Application.config.session_store :active_record_store
