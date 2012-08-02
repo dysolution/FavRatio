e2eaa1e35fff5ee20db24ce99457202a6f03ddbf
Jordan Peterson <dysolution@gmail.com>
2012-08-01 21:03:28 -0500

    begin testing single tweet view

--- spec/views/tweets/show.html.haml_spec.rb
+++ spec/views/tweets/show.html.haml_spec.rb
@@ -10,3 +10,13 @@ describe "tweets/show" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
   end
 end
+
+describe "tweets/show" do
+  it "displays the tweet content" do
+  assign(:tweet, stub_model(Tweet,
+    :text => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum sem mattis sapien blandit tempus. Sed velit ligula volutpat."
+  ))
+  render
+  rendered.should have_selector("#text", text: "Lorem ipsum")
+  end
+end
