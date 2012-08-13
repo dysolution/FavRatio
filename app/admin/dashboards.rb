1a643ee5629328642e0c015d3f65631f9754b4e3
Jordan Peterson <dysolution@gmail.com>
2012-08-12 20:54:06 -0500

    include Tweets in admin

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,5 +1,13 @@
 ActiveAdmin::Dashboards.build do
 
+  section "Tweets" do
+    ul do
+      Tweet.last(5).collect do |tweet|
+        li link_to(tweet.text, admin_tweet_path(tweet))
+      end
+    end
+  end
+
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
   # return the content which you would like to display.
